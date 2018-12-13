6a0397aa0a314736b96af724734d648b2dc4e920
Samuel Pelletier <pellesam.info@gmail.com>
2018-12-13 23:58:05 +0100

    gift

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
     $('#next').on('click',function(){
         $('body').html('');
+        $('body').append('<div id="circle"> <div id="gift"><div id="ribbon"></div><div id="giftbox"></div></div></div>');
+        $('body').append('<div id="circle"> <div id="gift"><div id="ribbon"></div><div id="giftbox"></div></div></div>');
+        $('body').append('<div id="circle"> <div id="gift"><div id="ribbon"></div><div id="giftbox"></div></div></div>');
+        $('body').append('<div id="circle"> <div id="gift"><div id="ribbon"></div><div id="giftbox"></div></div></div>');
     });
 })
+
